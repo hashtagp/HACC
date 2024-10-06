0308a808f3cdbe47b88fa51d26c3170b3349952a
Rachan Murthy <rachan3thi@gmail.com>
2024-10-06 09:19:10 +0530

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import { useState } from 'react'
-import { Routes,Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './App.css'
 
+import Layout from './routes/layout/layout'
+import Home from './routes/homePage/homePage'
+import Hackathon from './routes/hackathonPage/hackathonPage'
+import Coding from './routes/codingPage/codingPage'
+import Team from './routes/teamPage/teamPage'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />, 
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/hackathon",
+          element: <Hackathon />
+        },
+        {
+          path: "/coding",
+          element: <Coding />
+        },
+        {
+          path: "/team",
+          element: <Team />
+        }
+      ]
+    }
+  ]);
 
   return (
-    <>
-    <Navbar/>
-    <div>
-    <Routes>
-      <Route path='/' element={ <Home/> }/>
-      <Route path='/hackathon' element={ <Hackathon/> }/>
-      <Route path='/coding' element={ <Coding/> }/>
-      <Route path="/team" element={<Team/>}/>
-    </Routes>
-    </div>
-    <Footer/>
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
+
 
 export default App
